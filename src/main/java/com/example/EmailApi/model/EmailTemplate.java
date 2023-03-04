a965b344db9c9c7be0ce3e08adb96c30f1bba50a
Reena Kahar <reenabadera8999@gmail.com>
2023-03-04 17:44:25 +0530

    Email Api Demo

--- src/main/java/com/example/EmailApi/model/EmailTemplate.java
+++ src/main/java/com/example/EmailApi/model/EmailTemplate.java
@@ -3,10 +3,6 @@ package com.example.EmailApi.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
